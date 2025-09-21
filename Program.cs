a9f28858df1cb56233a481dbd99980d2684796e2
Jonna Barvsten <jonna.barvsten@gmail.com>
2025-09-21 12:45:17 +0200

    NumbersGame

--- Program.cs
+++ Program.cs
@@ -1,75 +1,219 @@
 ﻿using System;
+using System.Data;
 using System.Diagnostics.Metrics;
+using System.Runtime.Intrinsics.Arm;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NumbersGame
 {
     internal class Program
     {
+    
+        // Jonna Barvsten
+        // Fullstack.NET
 
-        public static string CheckGuessing(int guessNumber, int number)
+        // Returns messages based on how close the guess is to correct numnbe,
+        public static string CheckGuess(int guessNumber, int number)
         {
-           
-                if (guessNumber == number)
-                {
-                   
-                    return "WOHO you made it";
+           // Declare a integer that writes diffrent message
+            int diff = Math.Abs(guessNumber - number);
+            string message; 
 
-                }
-                else if (guessNumber < number)
-                {
-                    return "The number was to low!";
+            
+            if (diff == 0)
+            {
 
-                }
-                else
-                {
-                    return "You guess to high";
+                message = "WOHO you made it!";
 
-                }
             }
+            else if (diff <= 1)
+            {
+                message = "It is burning, you are very close!";
+
+            }
+            else if (diff <= 3)
+            {
+                message = "Hmm you are getting closer to the right number...";
+
+            }
+            else if (diff <= 10)
+            {
+                message = "It is very cold but on the right path!";
+            }
+            else 
+            {
+                message = "You are far far away!";
+
+            }
+
+            if (guessNumber < number) 
+            {
+                message += "\nYour guess is to low!";
+            
+            }
+            else if(guessNumber > number) 
+            {
+                message += "\nYour guess is to high!";
+            
+            }
+
+            return message;
+
+
+
+        }
+
+
+        // Returns how many possible numbers the user can guess based on difficulty level.
+        public static int LevelOfGame(int level) 
+        {   
+            
+            switch(level) 
+            {
+                case 1:
+                    return 10;
+
+                case 2:
+                    return 25;
+                case 3:
+                    return 50;
+                default:
+                    return -1;
+
+                
+            
+            }
+            
+
+        }
+
+        //Returns how many guess that the user is allowed to do. 
+        public static int UserTries(int tries) 
+        {
+            switch (tries) 
+            {
+                case 1:
+                    return 6;
+                case 2: 
+                    return 5;
+                case 3:
+                    return 3;
+                default:
+                    return -1;
+            
+            }
+            
+        
+        }
 
         static void Main(string[] args)
         {
-            // Print message to user. 
-            Console.WriteLine("Welcome! I'm thinking about a number. Can you guess which? You get five tries.");
-
-            var rnd = new Random();
-            int number = rnd.Next(1,21);
-            int guessNumber;
-            int tries = 0; 
-
+          
             
-            while ( tries < 5) 
-            { 
-            for (int i = 0; i < 5; i++)
+            while (true)
             {
-                Console.Write("Guess number: ");
-                bool sucess = int.TryParse(Console.ReadLine(), out guessNumber);
+                
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                // Meny to choose level. 
+                Console.WriteLine("Choose level to play game: \nPress 1 for EASY \nPress 2 for Medium \nPress 3 for HARD ");
+               
+                // Save userInput for level. 
+                int levelChoice = int.Parse(Console.ReadLine());
+            
+                
+                Console.Clear();
 
-                if (!sucess) 
+                 
+                int maxNumber = LevelOfGame(levelChoice);
+                int tries = UserTries(levelChoice);
+
+                // Handle invalid input.
+                if (maxNumber == -1 || tries == -1)
                 {
-                    Console.WriteLine("Please enter a number");
-                    // Gör att loopen börjar om istället för retunera 0 värde
+                    Console.WriteLine("Please choose a number between 1-3"); 
                     continue;
+                  
+                }
+            
+
+                // Start the game
+                Console.Write($"Welcome, I'm thinking about a number. Can you guess which?\nYou get {tries} tries. ");
+ 
+                var rnd = new Random();
+              
+                int number = rnd.Next(1, maxNumber + 1);
+                
+                int guessNumber;
+                
+                bool guessRight = false;
+
+                
+                Console.WriteLine($"I'm thinking about a number between 1 and {maxNumber}");
+
+
+                
+                for (int i = 0; i < tries;)
+                {
+                    
+                    Console.Write("Guess number: ");
+                   
+                    bool success = int.TryParse(Console.ReadLine(), out guessNumber);
+
+                    
+                    if (!success)
+                    {
+                        
+                        Console.WriteLine("Please enter a number");
+                        continue;
+
+
+                    }
+                    // Count valid attempts. 
+                    i++;
+
+                    
+                    string result = CheckGuess(guessNumber, number);
+                   
+                    
+                    Console.WriteLine(result);
+
+                    
+                    if (guessNumber == number)
+                    {
+
+                        guessRight = true; 
+                        break;
+                    }
                     
                 }
-
-                    tries++;
-
-               string result = CheckGuessing(guessNumber, number);
-
-                Console.WriteLine(result);
+                 
                 
-                if(guessNumber == number) 
-                    {
-                        return; 
-                    }
+                if (!guessRight)
+                {
+                   
+                    Console.WriteLine($"Sorry you failed guessing on {tries} times");
+                    Console.WriteLine($"The right number was: {number}");
+                }
+                
+                Console.ReadLine();
+                
+                Console.Clear();
 
+                
+                Console.WriteLine("Do you want to play again? Y/N?");
+                
+                string playAgain = Console.ReadLine().ToUpper();
+
+                
+                if (playAgain == "N")
+                {
+                    Console.WriteLine("Thanks for playing");
+                    Console.ReadLine(); 
+                    break; 
+                }
+                // Clear consol window for new game. 
+                Console.Clear();
             }
-
-            }
-
-            Console.WriteLine("Sorry you failed guessing on 5 times");
 
 
 
@@ -81,3 +225,11 @@ namespace NumbersGame
         
     }
 }
+
+
+
+
+
+
+
+
